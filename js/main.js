9743b46355c4f08963837ad6804949d7ffe40c7a
Nikita0802 <nikita.08022000@gmail.com>
2022-06-20 18:26:57 +0300

    new slider

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
-$(function(){
-
+$(function () {
 
 
     $('.slider__inner').slick({
-        arrows: false, 
+        arrows: false,
         dots: true,
     });
 
-
-  });
+    $('.works__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/left-shape.svg" alt=""></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/right-shape.svg" alt=""></button>'
+    });
+});
